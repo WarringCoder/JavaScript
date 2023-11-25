3a5af2bb302eca5a24adf30ab379b2e3fa0ec2d3
İshak Eren <ishakerenn1905@gmail.com>
2023-11-25 15:51:06 +0300

    completed

--- Medium_Project/Music_Player/music.js
+++ Medium_Project/Music_Player/music.js
@@ -15,5 +15,6 @@ const MusicList =
 [
     new Music("Padrino / Nino Rota", "1972" ,"1.webp","1.mp3"),
     new Music("Peaky Blinder / Otnicka - Duke Lika", "2019" ,"2.jpeg", "2.mp3"),
-    new Music("Carmina Burana /  Carl Orff", "1936" ,"3.jpeg", "3.mp3")
+    new Music("Carmina Burana /  Carl Orff", "1936" ,"3.jpeg", "3.mp3"),
+    new Music("Radioactive / İmagine Dragon", "2013", "4.jpg", "4.mp3")
 ]
